5ece8dfb7efb27937df4a356953103c3bef72073
RakshithTHBS <40590349+RakshithTHBS@users.noreply.github.com>
2020-02-04 15:56:15 +0530

    Add files via upload

---
+++ spring-security-basics-demo/src/main/java/com/rakshith/springsecurity/demo/controller/ResourceController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.rakshith.springsecurity.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rakshith_rathnakara
+ *
+ */
+@RestController
+public class ResourceController {
+	
+	@GetMapping("/")
+	public String getResource() {
+		return ("<h1>welcome<h1>");
+	}
+	
+	
+	@GetMapping("/admin")
+	public String getAdmin() {
+		return ("<h1>welcome Admin<h1>");
+	}
+	
+	@GetMapping("/user")
+	public String getUser() {
+		return ("<h1>welcome user<h1>");
+	}
+
+}
